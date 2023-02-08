a9e63f1be9c471b501d5aaf33a58cda50893708d
Jack Danzey <jackdanzey@gmail.com>
2023-02-08 08:40:30 -0500

    turned the parser into a function. data is no longer static, but run through the terminal

--- rss.py
+++ rss.py
@@ -1,5 +1,16 @@
 import feedparser
 
-rss_url = 'https://timesofindia.indiatimes.com/rssfeedstopstories.cms'
-rss = feedparser.parse(rss_url)
-print(rss)
+rss_url = input(
+    'Please enter the url for the rss feed that you would like to see:')
+
+# https: // timesofindia.indiatimes.com/rssfeedstopstories.cms
+
+def feed(rss):
+    rss = feedparser.parse(rss)
+    print('Here is your RSS feed:')
+    print('Title: ' + rss.feed.title)
+    print('Link: ' + rss.feed.link)
+    print('Description: ' + rss.feed.description)
+
+
+feed(rss_url)
